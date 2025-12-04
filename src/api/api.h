17954385996efaf16db69ec88e573b4d2109cf66
Fathi Helmi <fathihelmi@outlook.com>
2025-12-05 00:44:02 +0100

    feat: added support to text rendering for clay and expanded layout config

--- src/api/api.h
+++ src/api/api.h
@@ -9,7 +9,8 @@
 #define API_TYPE_NATIVE_PLUGIN "NativePlugin"
 #define API_TYPE_RENWINDOW "RenWindow"
 
-#define API_CONSTANT_DEFINE(L, idx, key, n) (lua_pushnumber(L, n), lua_setfield(L, idx - 1, key))
+#define API_CONSTANT_DEFINE(L, idx, key, n) \
+  (lua_pushnumber(L, n), lua_setfield(L, idx - 1, key))
 
 void api_load_libs(lua_State *L);
 
--- src/clay_rencache.hpp
+++ src/clay_rencache.hpp
@@ -1,11 +1,18 @@
 #pragma once
 
+#include <array>
+#include <vector>
+
 extern "C" {
 #include "clay.h"
 #include "renderer.h"
 }
 
 namespace clay {
+
+struct FontGroup {
+    std::array<RenFont*, FONT_FALLBACK_MAX> fonts;
+};
 
 // birdge layer between Clay and rencache.h
 class ClayRencache {
@@ -17,6 +24,10 @@ public:
     
     // Check if a Clay element should be rendered
     static bool ShouldRender(const Clay_BoundingBox& box, const Clay_BoundingBox& clipRect);
+
+    static uint16_t AddFont(const FontGroup& fontGroup);
+    static FontGroup* GetFont(uint16_t id);
+    static void ResetFonts();
     
 private:
     static bool initialized;
